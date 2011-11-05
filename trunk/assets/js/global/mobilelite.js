4f8e208a83eefcaf630c0be419cc4b57f61163ef
jlj008 <jlj008@79b3bfa4-1519-f35f-524d-781c85d310dd>
2011-11-05 10:08:41 +0000

    Pass business bean definition to web view.

--- trunk/assets/js/global/mobilelite.js
+++ trunk/assets/js/global/mobilelite.js
@@ -1,21 +1,26 @@
-(function(){
+(function() {
 
 var MobileLiteEngine = function(mobileLite) {
-	};
+};
 
 MobileLiteEngine.prototype = {
 	constructor: MobileLiteEngine,
-	createLiteProxy: function (obj) {
-		window[obj] = {
-			name: obj,
-			engine: this,
-			__noSuchMethod__: function(methodName, args) {
-				this[methodName] = function(args) {
-					this.engine.invokeBeanAction(this.name, methodName, args);
-				}
-				this[methodName](args);
-			}
+	createLiteProxy: function(obj) {
+		window[obj.name] = {
+			name: obj.name,
+			engine: this
+//			__noSuchMethod__: function(methodName, args) {
+//				this[methodName] = function(args) {
+//					this.engine.invokeBeanAction(this.name, methodName, args);
+//				}
+//				this[methodName](args);
+//			}
 		};
+		for (methodName in obj.methodNames) {
+			window[obj.name][obj.methodNames[methodName]] = function(args) {
+				this.engine.invokeBeanAction(this.name, obj.methodNames[methodName], args);
+			}
+		}
 	},
 	invokeBeanAction: function(bean, methodName, args) {
 		alert( "bean:" + bean + ", method:" + methodName + ", args:" + args );
@@ -23,21 +28,20 @@ MobileLiteEngine.prototype = {
 };
 
 var mobileLite = {
-		engine: new MobileLiteEngine(),
-		initBeans: function(beans) {
-			for ( bean in beans) {
-				this.engine.createLiteProxy(beans[bean]);
-			}
+	engine: new MobileLiteEngine(),
+	initBeans: function(beans) {
+		for (bean in beans) {
+			this.engine.createLiteProxy(beans[bean]);
 		}
-	};
+	}
+};
 
 if (typeof exports !== 'undefined') exports.mobileLite = mobileLite;
 else window.mobileLite = mobileLite;
 
-})();
+}) ();
 
 $(document).ready(function() {
-	//_mobileLiteProxy_.onPageReady();
-	mobileLite.initBeans(["bean"]);
+	_mobileLiteProxy_.onPageReady();
+	//mobileLite.initBeans(["bean"]);
 });
-

c471651d869e0c2163e927f79b31323971389b19
nomadshark2000@gmail.com <nomadshark2000@gmail.com@79b3bfa4-1519-f35f-524d-781c85d310dd>
2011-11-05 14:18:39 +0000

    draft commit for discussion

--- assets/js/global/mobilelite.js
+++ assets/js/global/mobilelite.js
@@ -5,25 +5,34 @@ var MobileLiteEngine = function(mobileLite) {
 
 MobileLiteEngine.prototype = {
 	constructor: MobileLiteEngine,
+	guid: 1, 
 	createLiteProxy: function(obj) {
 		window[obj.name] = {
 			name: obj.name,
 			engine: this
-//			__noSuchMethod__: function(methodName, args) {
-//				this[methodName] = function(args) {
-//					this.engine.invokeBeanAction(this.name, methodName, args);
-//				}
-//				this[methodName](args);
-//			}
 		};
 		for (methodName in obj.methodNames) {
-			window[obj.name][obj.methodNames[methodName]] = function(args) {
-				this.engine.invokeBeanAction(this.name, obj.methodNames[methodName], args);
+			window[obj.name][obj.methodNames[methodName]] = function() {
+				var args = Array.prototype.slice.call(arguments);
+				var callback = null;
+				if(args.length >0 && args[args.length - 1] instanceof Function) {
+					callback = args[args.length - 1];
+					args = args.slice(0, args.length - 1);
+				}
+				
+				if(callback && !(callback.guid)) {
+					callback.guid = this.engine.guid++;
+					mobileLite.callback[callback.guid] = callback;
+				}
+				
+				this.engine.invokeBeanAction(this.name, obj.methodNames[methodName], args, callback.toString());
 			}
 		}
 	},
-	invokeBeanAction: function(bean, methodName, args) {
-		alert( "bean:" + bean + ", method:" + methodName + ", args:" + args );
+	invokeBeanAction: function(bean, methodName, args, callback) {
+		//alert( "bean:" + bean + ", method:" + methodName + ", args:" + args );
+		_mobileLiteProxy_.invokeBeanAction(bean, methodName, args, callback);
+		//alert( "bean:" + bean + ", method:" + methodName + ", args:" + args + ", callback:" + callback );
 	}
 };
 
@@ -33,7 +42,8 @@ var mobileLite = {
 		for (bean in beans) {
 			this.engine.createLiteProxy(beans[bean]);
 		}
-	}
+	},
+	callback: {}
 };
 
 if (typeof exports !== 'undefined') exports.mobileLite = mobileLite;

ddfc4aef0911391217332e0f609c61d2ac340fc9
nomadshark2000@gmail.com <nomadshark2000@gmail.com@79b3bfa4-1519-f35f-524d-781c85d310dd>
2011-11-06 11:51:20 +0000

    correct demo

--- trunk/assets/js/page/demo.js
+++ trunk/assets/js/page/demo.js
@@ -2,10 +2,10 @@ MOBILELITE.libs.readyBody = function() {
 	$("#topNavPassengerInfo").click(function() {
 		bean.show("你好Tony!", function() {alert('aaa')});
 		bean.show("你好Tony!!");
-		bean.queryContact({id:'q1', name:'qJim'}, function(jsonStr) {
-			alert(jsonStr);
+		bean.queryContact({id:'q1', name:'qJim'}, function(contact) {
+//			alert(jsonStr);
 //			var contact = eval("(" + unescape(jsonStr) + ")");
-//			alert("query result: id-" + contact.id + ", name-" + contact.name);
+			alert("query result: id-" + contact.id + ", name-" + contact.name);
 		});
 	});
 }

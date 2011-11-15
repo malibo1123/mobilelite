25f0ba50d9c1dce899383f20deb2b9241aeb3c62
nomadshark2000@gmail.com <nomadshark2000@gmail.com@79b3bfa4-1519-f35f-524d-781c85d310dd>
2011-11-15 13:55:24 +0000

    [maven-scm] copy for tag mobilelite-core-0.0.1

---
+++ tags/mobilelite-core-0.0.1/src/main/java/org/mobilelite/android/ServiceBean.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2011 Tony.Ni, Jim.Jiang http://mobilelite.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mobilelite.android;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mobilelite.annotation.ServiceMethod;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+public class ServiceBean {
+	
+	private String name;
+	private Object bean;
+
+	public ServiceBean(String name, Object bean) {
+		this.name = name;
+		this.bean = bean;
+	}
+
+	public ServiceBeanDefinition getServiceBeanDefinition() {
+		return ServiceBeanDefinition.newInstance(name, bean);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Object invoke(String methodName, JsonElement jsonParam) {
+		Object result = null;
+		Gson gson = new Gson();
+		JsonElement je = jsonParam;
+		
+		JsonArray jaParams = null;
+		if (je.isJsonArray()) {
+			jaParams = je.getAsJsonArray();
+		} else if (je.isJsonObject() || je.isJsonPrimitive()) {
+			jaParams = new JsonArray();
+			jaParams.add(je);
+		} else if (je.isJsonNull()) {
+			jaParams = new JsonArray();
+		}
+
+		Method[] methods = getBeanMethods(methodName, jaParams.size());
+		for (Method method : methods) {
+			@SuppressWarnings("rawtypes")
+			Class[] paramClasses = method.getParameterTypes();
+			List<Object> params = new ArrayList<Object>();
+			try {
+				for (int i = 0; i < paramClasses.length; i++) {
+					params.add(gson.fromJson(jaParams.get(i), paramClasses[i]));
+				}
+				
+				result = method.invoke(bean, params.toArray());
+				break;
+			} catch (JsonParseException e) {
+			} catch (IllegalArgumentException e) {
+			} catch (IllegalAccessException e) {
+			} catch (InvocationTargetException e) {
+			}
+		}
+
+		return result;
+	}
+
+	private Method[] getBeanMethods(String methodName, int paramNum) {
+		List<Method> methods = new ArrayList<Method>();
+		Method[] beanMethods = bean.getClass().getDeclaredMethods();
+		for (Method method : beanMethods) {
+			if (method.isAnnotationPresent(ServiceMethod.class) 
+					&& method.getName().equals(methodName) 
+					&& method.getParameterTypes().length == paramNum)
+				methods.add(method);
+		}
+		return methods.toArray(new Method[]{});
+	}
+
+}
---
+++ tags/mobilelite-core-0.0.1/src/main/java/org/mobilelite/android/ServiceBeanDefinition.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2011 Tony.Ni, Jim.Jiang http://mobilelite.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mobilelite.android;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.mobilelite.annotation.Service;
+import org.mobilelite.annotation.ServiceMethod;
+
+public class ServiceBeanDefinition {
+
+	private String name;
+	
+	private List<String> methodNames = new ArrayList<String>();
+	
+	private Object bean;
+	
+	public static ServiceBeanDefinition newInstance(String name, Object bean) {
+		if (bean.getClass().isAnnotationPresent(Service.class)) {
+			// is service object, getting its bean definition
+			return new ServiceBeanDefinition(name, bean);
+		} else {
+			return null;
+		}
+	}
+	
+	private ServiceBeanDefinition(String name, Object bean) {
+		this.name = name;
+		this.bean = bean;
+		initMethodNames();
+	}
+	
+	private void initMethodNames() {
+		Method[] methods = bean.getClass().getDeclaredMethods();
+		for (int i = 0; i < methods.length; i++) {
+			if (methods[i].isAnnotationPresent(ServiceMethod.class)) {
+				// is service method, need to be exposed to definition
+				String methodName = methods[i].getName();
+				if (!methodNames.contains(methodName)) {
+					methodNames.add(methods[i].getName());
+				}
+			}
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public List<String> getMethodNames() {
+		return Collections.unmodifiableList(methodNames);
+	}
+
+}
